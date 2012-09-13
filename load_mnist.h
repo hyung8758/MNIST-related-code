3bca37fd63fd04b0cffc76cef838cc2340f1fc3a
Yitao Li <yl790@nyu.edu>
2012-09-13 11:35:11 -0700

    test code for load_mnist

--- load_mnist.h
+++ load_mnist.h
@@ -111,22 +111,4 @@ int display_mnist_digit(unsigned int width, unsigned int height, unsigned char *
 	return 0;
 }
 
-/*
-
-//test code
-
-int main(int argc, char *argv[]){
-	unsigned int width, height;
-	std::vector<unsigned char **> mnist_digits[10];
-	if (argc == 3){
-		load_mnist(argv[1], argv[2], width, height, mnist_digits);
-		display_mnist_digit(width, height, mnist_digits[9][0]);
-		free_mnist(height, mnist_digits);
-		return 0;
-	}
-	std::cout<<"Usage: "<<argv[0]<<" <name of image file> <name of label file>\n";
-	return 0;
-}
-*/
-
 #endif
---
+++ test_load_mnist.c
@@ -0,0 +1,14 @@
+#include "load_mnist.h"
+
+int main(int argc, char *argv[]){
+	unsigned int width, height;
+	std::vector<unsigned char **> mnist_digits[10];
+	if (argc == 3){
+		load_mnist(argv[1], argv[2], width, height, mnist_digits);
+		display_mnist_digit(width, height, mnist_digits[9][0]);
+		free_mnist(height, mnist_digits);
+		return 0;
+	}
+	std::cout<<"Usage: "<<argv[0]<<" <name of image file> <name of label file>\n";
+	return 0;
+}
